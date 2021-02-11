de0776fecce04984298e0cff5bbeeb0463b152fe
Igor Gabriel <igabrields@gmail.com>
2021-02-11 12:42:35 -0300

    Corrigindo casos de testes

--- src/test/kotlin/com/igor/challenge/core/service/implementations/ValidatorResourcesServiceTest.kt
+++ src/test/kotlin/com/igor/challenge/core/service/implementations/ValidatorResourcesServiceTest.kt
@@ -1,7 +1,9 @@
 package com.igor.challenge.core.service.implementations
 
 import com.igor.challenge.core.entities.DataTransferRequestPassword
+import com.igor.challenge.core.factory.ValidatorFactory
 import com.igor.challenge.core.service.ValidatorResourcesService
+import com.igor.challenge.core.utils.RulesType
 import com.igor.challenge.core.utils.ValidatorMessages
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
@@ -16,72 +18,98 @@ internal class ValidatorResourcesServiceTest {
     private lateinit var service: ValidatorResourcesService
 
     @Test
-    fun blankSpaceRuleTest() {
-        val password: DataTransferRequestPassword = DataTransferRequestPassword(
-                password = "AbTp9 fok"
-        )
-        val responseValidation = service.executeValidatorPasswordAndFilter(password)
-        assertFalse(responseValidation.isValid)
-        assertTrue(responseValidation.inconsistencies.filter { it.equals(ValidatorMessages.NON_EMPTY_CHARACTERS_REQUIRED) }.any())
-    }
+    fun executeValidatorPasswordAndFilterTest() {
 
-    @Test
-    fun emptyRuleTest() {
-        val password: DataTransferRequestPassword = DataTransferRequestPassword(
-                password = ""
-        )
-        val responseValidation = service.executeValidatorPasswordAndFilter(password)
-        assertFalse(responseValidation.isValid)
-        assertTrue(responseValidation.inconsistencies.filter { it.equals(ValidatorMessages.NON_EMPTY_REQUIRED) }.any())
-    }
-
-    @Test
-    fun lowerCaseCharacterRuleTest() {
         val password: DataTransferRequestPassword = DataTransferRequestPassword(
                 password = "ABT9FOKW!"
         )
         val responseValidation = service.executeValidatorPasswordAndFilter(password)
         assertFalse(responseValidation.isValid)
         assertTrue(responseValidation.inconsistencies.filter { it.equals(ValidatorMessages.LOWER_CASE_CHARACTERS_REQUIRED) }.any())
+
+    }
+
+    @Test
+    fun blankSpaceRuleTest() {
+
+        val validationStatus = ValidatorFactory
+                .createValidator(RulesType.BLANK_SPACE)
+                .executeRule("AbTp9 fok")
+
+        assertFalse(validationStatus.isValid)
+        assertTrue(validationStatus.message == ValidatorMessages.NON_EMPTY_CHARACTERS_REQUIRED)
+
+    }
+
+    @Test
+    fun emptyRuleTest() {
+
+        val validationStatus = ValidatorFactory
+                .createValidator(RulesType.EMPTY)
+                .executeRule("")
+
+        assertFalse(validationStatus.isValid)
+        assertTrue(validationStatus.message == ValidatorMessages.NON_EMPTY_REQUIRED)
+
+    }
+
+    @Test
+    fun lowerCaseCharacterRuleTest() {
+
+        val validationStatus = ValidatorFactory
+                .createValidator(RulesType.LOWER_CASE_CHARACTERS)
+                .executeRule("ABT9FOKW!")
+
+        assertFalse(validationStatus.isValid)
+        assertTrue(validationStatus.message == ValidatorMessages.LOWER_CASE_CHARACTERS_REQUIRED)
+
     }
 
     @Test
     fun minumumCharacterRuleTest() {
-        val password: DataTransferRequestPassword = DataTransferRequestPassword(
-                password = "AAAbbbC!"
-        )
-        val responseValidation = service.executeValidatorPasswordAndFilter(password)
-        assertFalse(responseValidation.isValid)
-        assertTrue(responseValidation.inconsistencies.filter { it.equals(ValidatorMessages.MININUM_CHARACTERS_REQUIRED) }.any())
+
+        val validationStatus = ValidatorFactory
+                .createValidator(RulesType.MINIMUM_CHARACTERS)
+                .executeRule("AAAbbbC!")
+
+        assertFalse(validationStatus.isValid)
+        assertTrue(validationStatus.message == ValidatorMessages.MININUM_CHARACTERS_REQUIRED)
+
     }
 
     @Test
     fun repeatableCharacterRuleTest() {
-        val password: DataTransferRequestPassword = DataTransferRequestPassword(
-                password = "AAAbbbC!w"
-        )
-        val responseValidation = service.executeValidatorPasswordAndFilter(password)
-        assertFalse(responseValidation.isValid)
-        assertTrue(responseValidation.inconsistencies.filter { it.equals(ValidatorMessages.NON_REPEATABLE_CHARACTERS_REQUIRED) }.any())
+
+        val validationStatus = ValidatorFactory
+                .createValidator(RulesType.REPETEABLE_CHARACTERS)
+                .executeRule("AAAbbbC!w")
+
+        assertFalse(validationStatus.isValid)
+        assertTrue(validationStatus.message == ValidatorMessages.NON_REPEATABLE_CHARACTERS_REQUIRED)
+
     }
 
     @Test
     fun specialCharacterRuleTest() {
-        val password: DataTransferRequestPassword = DataTransferRequestPassword(
-                password = "AAAbbbCc"
-        )
-        val responseValidation = service.executeValidatorPasswordAndFilter(password)
-        assertFalse(responseValidation.isValid)
-        assertTrue(responseValidation.inconsistencies.filter { it.equals(ValidatorMessages.SPECIAL_CHARACTERS_REQUIRED) }.any())
+
+        val validationStatus = ValidatorFactory
+                .createValidator(RulesType.CONTAINS_SPECIAL_CHARACTERS)
+                .executeRule("AAAbbbCc")
+
+        assertFalse(validationStatus.isValid)
+        assertTrue(validationStatus.message == ValidatorMessages.SPECIAL_CHARACTERS_REQUIRED)
+
     }
 
     @Test
     fun upperCaseCharacterRuleTest() {
-        val password: DataTransferRequestPassword = DataTransferRequestPassword(
-                password = "aaabbbcc!"
-        )
-        val responseValidation = service.executeValidatorPasswordAndFilter(password)
-        assertFalse(responseValidation.isValid)
-        assertTrue(responseValidation.inconsistencies.filter { it.equals(ValidatorMessages.UPPER_CASE_CHARACTERS_REQUIRED) }.any())
+
+        val validationStatus = ValidatorFactory
+                .createValidator(RulesType.UPPER_CASE_CHARACTERS)
+                .executeRule("aaabbbcc")
+
+        assertFalse(validationStatus.isValid)
+        assertTrue(validationStatus.message == ValidatorMessages.UPPER_CASE_CHARACTERS_REQUIRED)
+
     }
 }
